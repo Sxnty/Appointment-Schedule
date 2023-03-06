3d9f3f213c583fab4c3383f75cfdf0d3a5f7f28c
Sxnty <sxntycsgo@gmail.com>
2023-03-06 07:51:36 -0300

    stylized edit appointment component

--- src/componenets/EditAppointment.jsx
+++ src/componenets/EditAppointment.jsx
@@ -5,7 +5,6 @@ import { AppointmentsContext } from "../context/AppointmentsContext";
 import "../styles/editAppointment.css";
 function EditAppointment() {
   const { appointments } = useContext(AppointmentsContext);
-  console.log(appointments);
   const { id } = useParams();
   let thisAppoint = appointments.filter((e) => e.id === id);
   console.log(thisAppoint);
@@ -14,32 +13,34 @@ function EditAppointment() {
     <>
       <section className="edit__main">
         <div className="edit__container">
-          <article className="edit__time">
-            <div className="edit__header">
+          <article className="edit__header">
             <h2>Modify appointment from:</h2>
             <span>{thisAppoint[0].name}</span>
-            </div>
-            <div className="time__date">
-              <p>Change Date:</p>
-              <input type="date" />
-            </div>
-            <div className="time__hour">
-              <p>Change Hour:</p>
-              <input type="time" />
-            </div>
           </article>
-          <article className="edit__info">
-            <div className="info__name">
-              <p>Change Name</p>
-              <input type="text" defaultValue={thisAppoint[0].name} />
-            </div>
-            <div className="info__description">
-              <p>Change Description</p>
-              <textarea
-                name="description"
-                defaultValue={thisAppoint[0].description}
-              ></textarea>
-            </div>
+          <article className="fieldset__container">
+            <article className="edit__time">
+              <div className="time__date">
+                <p>Change Date:</p>
+                <input type="date" />
+              </div>
+              <div className="time__hour">
+                <p>Change Hour:</p>
+                <input type="time" />
+              </div>
+            </article>
+            <article className="edit__info">
+              <div className="info__name">
+                <p>Change Name</p>
+                <input type="text" defaultValue={thisAppoint[0].name} />
+              </div>
+              <div className="info__description">
+                <p>Change Description</p>
+                <textarea
+                  name="description"
+                  defaultValue={thisAppoint[0].description}
+                ></textarea>
+              </div>
+            </article>
           </article>
           <button>Edit</button>
         </div>
